2cb881e41918b92f1885d9bcdb3ef02535967e39
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-11-10 22:51:32 +0200

    Password hashing added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const router = Router()
 
@@ -7,7 +8,18 @@ router.post('/register', async (req, res) => {
 
         const {email, password} = req.body
 
-        
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            return res.status(400).json({message: 'User already exists'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({ email, password: hashedPassword })
+
+        await user.save()
+
+        res.status(201).json({ message: 'User was created' })
 
     } catch (e) {
         res.status(500).json({ message: 'Something went wrong...please try again later' })
